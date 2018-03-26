6fa664ae2eba6af9f92c1e4fdaa52db145963a89
Alexey Kott <alexey.kott@gmail.com>
2018-03-26 23:18:29 +0300

    848 done

--- 848.c
+++ 848.c
@@ -4,33 +4,16 @@
 #include <stdbool.h>
 #include <malloc.h>
 
-// char* clear(char* STR){
-//     STR[0] = '\0';
-//     return STR;
-// }
 
-bool is_digit(char c){
-    if(((int)c>47) && ((int)c < 58)){
-        return true;
-    }
-    return false;
-}
-
-char* clear(char* STR){
-    STR[0] = '\0';
-    for(int i=0;i<strlen(STR);i++){
-        STR[i]='\0';
-    }
-
-    return STR;
-}
+bool is_digit(char c);
+void empty(char* STR);
 
 int main(void)
 {
     FILE* fp;
-    char* STR = NULL;
+    char *STR = NULL, *BFR;
     size_t len = 0;
-    int i, j;
+    int i, j, n;
 
     fp = fopen("input.txt", "r");
 
@@ -38,21 +21,45 @@ int main(void)
 
     fclose(fp);
 
-    char* BUFFER = calloc(strlen(STR), sizeof(char));
-    for(i=0, j=0;i<=strlen(STR);i++){
-        if(is_digit(STR[i])){
-            // printf("%c", STR[i]);
-            BUFFER[j]=STR[i];
-            j++;
-        }else{
-            printf("%s\n", BUFFER);
-            BUFFER = clear(BUFFER);
-            // printf("%s\n", BUFFER);
-            j=0;
+    BFR = calloc(strlen(STR), sizeof(char));
 
+    for (i = 0, j = 0; i <= strlen(STR); i++)
+    {
+        if (is_digit(STR[i]))
+        {
+            BFR[j] = STR[i];
+            j++;
+        }
+        else
+        {
+            n = (int)strtol(BFR, (char**)NULL, 10);
+            if (n > 5)
+            {
+                printf("NO");
+                return 0;
+            }
+
+            empty(BFR);
+            j = 0;
         }
     }
+
+    printf("YES");
 
     return 0;
 }
 
+bool is_digit(char c)
+{
+    if (((int)c > 47) && ((int)c < 58))
+    {
+        return true;
+    }
+    return false;
+}
+
+
+void empty(char* STR)
+{
+    memset(STR, 0, strlen(STR) * sizeof(char));
+}
