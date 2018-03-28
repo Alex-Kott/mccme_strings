7c121f246a7aac01b14de4e5bfc72ef24215477e
Alexey Kott <alexey.kott@gmail.com>
2018-03-28 10:33:13 +0300

    dcsdcsd

---
+++ 1026.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <malloc.h>
+
+void empty(char *STR);
+int calc(char *MNML, int x);
+bool is_digit(char c);
+
+int main(void)
+{
+    FILE* fp;
+    char *STR, *MNML; // monomial
+    size_t len = 0;
+    int i, j, x, res;
+    char sign;
+
+    fp = fopen("input.txt", "r");
+
+    getline(&STR, &len, fp);
+    fscanf(fp, "%d\n", &x);
+    fclose(fp);
+
+
+    MNML = calloc(10, sizeof(char));
+
+    for(i=0, res=0, j++;i<strlen(STR);i++){
+        if(STR[i] == '+' || STR[i] == '-'){
+            MNML[j] = '\0';
+
+            if(sign == '+'){
+                res = res + calc(MNML, x);
+            }else{
+                res = res - calc(MNML, x);
+            }
+
+            sign = STR[i];
+            j=0;
+            empty(MNML);
+
+        } else {
+            MNML[j] = STR[i];
+            j++;
+
+        }
+    }
+
+    // printf("%i\n", res);
+    return 0;
+}
+
+
+void empty(char* STR)
+{
+    memset(STR, 0, strlen(STR) * sizeof(char));
+}
+
+int calc(char *MNML, int x){
+    if(strlen(MNML) == 0){
+        return 0;
+    }
+
+    int i, j;
+
+    printf("%s ", MNML);
+    char *N = calloc(5, sizeof(char));
+    N[0] = '1';
+    N[1] = '\0';
+    char *PWR = calloc(5, sizeof(char));
+    PWR[0] = '1';
+    PWR[1] = '\0';
+    bool flag = true;
+    for(i=0, j=0; i<strlen(MNML); i++){
+        if(is_digit(MNML[i])){
+            if(flag == true){
+                N[j]=MNML[i];    
+            }else{
+                PWR[j]=MNML[i];
+            }
+            j++;
+            
+        }else{
+            N[j] = '\0';
+            flag = false;
+            j=0;
+        }
+    }
+    printf("%i \n", (int)N);
+    empty(N);
+    empty(PWR);
+    return 1;
+}
+
+bool is_digit(char c)
+{
+    if (((int)c > 47) && ((int)c < 58))
+    {
+        return true;
+    }
+    return false;
+}
